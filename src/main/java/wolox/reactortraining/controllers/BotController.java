8184bce0c3d4f2c8360ba62ba43922ec59d244af
Darío Peña <leonplondon@gmail.com>
2019-10-23 09:20:21 -0500

    Added annotated status code to create bot endpoint

--- src/main/java/wolox/reactortraining/controllers/BotController.java
+++ src/main/java/wolox/reactortraining/controllers/BotController.java
@@ -3,12 +3,14 @@ package wolox.reactortraining.controllers;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 import wolox.reactortraining.dtos.BotCreationDto;
@@ -42,6 +44,7 @@ public class BotController {
     }
 
     @PostMapping("/create")
+    @ResponseStatus(code = HttpStatus.OK, reason = "Bot created successfully")
     public Mono<Void> createBot(@Valid @RequestBody BotCreationDto botCreationDto) {
         return botService
             .createBot(botCreationDto)
